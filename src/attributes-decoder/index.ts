5a899a0850fb13ccf33e91f36098a139023c2a1f
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-05-26 10:25:55 +0300

    SERVICES-110 add staking tokens attributes decoders
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/attributes-decoder/index.ts
+++ src/attributes-decoder/index.ts
@@ -1,3 +1,4 @@
 export * from './locked-assets';
 export * from './farm';
 export * from './proxy';
+export * from './staking';
---
+++ src/attributes-decoder/staking/staking.farm.token.ts
@@ -0,0 +1,60 @@
+import {
+    BigUIntType,
+    BinaryCodec,
+    FieldDefinition,
+    StructType,
+} from '@elrondnetwork/erdjs/out';
+import {
+    StakingFarmTokenAttributesType,
+    StakingFarmTokenType,
+} from './staking.farm.token.types';
+
+export class StakingFarmTokenAttributes {
+    type = StakingFarmTokenType.STAKING_FARM_TOKEN;
+    rewardPerShare: string | undefined;
+    compoundedReward: string | undefined;
+    currentFarmAmount: string | undefined;
+
+    constructor(init?: Partial<StakingFarmTokenAttributes>) {
+        Object.assign(this, init);
+    }
+
+    toJSON(): StakingFarmTokenAttributesType {
+        return {
+            type: this.type,
+            rewardPerShare: this.rewardPerShare,
+            compoundedReward: this.compoundedReward,
+            currentFarmAmount: this.currentFarmAmount,
+        };
+    }
+
+    static fromDecodedAttributes(
+        decodedAttributes: any,
+    ): StakingFarmTokenAttributes {
+        return new StakingFarmTokenAttributes({
+            rewardPerShare: decodedAttributes.rewardPerShare.toFixed(),
+            compoundedReward: decodedAttributes.compoundedReward.toFixed(),
+            currentFarmAmount: decodedAttributes.currentFarmAmount.toFixed(),
+        });
+    }
+
+    static fromAttributes(attributes: string): StakingFarmTokenAttributes {
+        const attributesBuffer = Buffer.from(attributes, 'base64');
+        const codec = new BinaryCodec();
+
+        const structType = StakingFarmTokenAttributes.getStructure();
+        const [decoded] = codec.decodeNested(attributesBuffer, structType);
+
+        return StakingFarmTokenAttributes.fromDecodedAttributes(
+            decoded.valueOf(),
+        );
+    }
+
+    static getStructure(): StructType {
+        return new StructType('StakingFarmTokenAttributes', [
+            new FieldDefinition('rewardPerShare', '', new BigUIntType()),
+            new FieldDefinition('compoundedReward', '', new BigUIntType()),
+            new FieldDefinition('currentFarmAmount', '', new BigUIntType()),
+        ]);
+    }
+}
