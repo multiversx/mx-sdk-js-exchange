bdfb2a7a770fb2c526e9012738d565f3dcbae806
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-09-22 11:15:56 +0300

    SERVICES-635 add energy attributes decoder
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/attributes-decoder/energy/energy.ts
@@ -0,0 +1,58 @@
+import {
+    BigUIntType,
+    BinaryCodec,
+    FieldDefinition,
+    StructType,
+    U64Type,
+} from '@elrondnetwork/erdjs/out';
+import BigNumber from 'bignumber.js';
+import { EnergyType } from './energy.type';
+
+export class Energy {
+    amount: BigNumber;
+    lastUpdateEpoch: number;
+    totalLockedTokens: BigNumber;
+
+    constructor(init: {
+        amount: BigNumber;
+        lastUpdateEpoch: number;
+        totalLockedTokens: BigNumber;
+    }) {
+        this.amount = init.amount;
+        this.lastUpdateEpoch = init.lastUpdateEpoch;
+        this.totalLockedTokens = init.totalLockedTokens;
+    }
+
+    toJSON(): EnergyType {
+        return {
+            amount: this.amount.toFixed(),
+            lastUpdateEpoch: this.lastUpdateEpoch,
+            totalLockedTokens: this.totalLockedTokens.toFixed(),
+        };
+    }
+
+    static fromDecodedAttributes(decodedAttributes: any): Energy {
+        return new Energy({
+            amount: decodedAttributes.amount,
+            lastUpdateEpoch: decodedAttributes.last_update_epoch,
+            totalLockedTokens: decodedAttributes.total_locked_tokens,
+        });
+    }
+
+    static fromAttributes(attributes: string): Energy {
+        const attributesBuffer = Buffer.from(attributes, 'base64');
+        const codec = new BinaryCodec();
+
+        const structType = Energy.getStructure();
+        const [decoded] = codec.decodeNested(attributesBuffer, structType);
+
+        return Energy.fromDecodedAttributes(decoded.valueOf());
+    }
+    static getStructure(): StructType {
+        return new StructType('Energy', [
+            new FieldDefinition('amount', '', new BigUIntType()),
+            new FieldDefinition('last_update_epoch', '', new U64Type()),
+            new FieldDefinition('total_locked_tokens', '', new BigUIntType()),
+        ]);
+    }
+}
