cef1d806b8d4d88db2c925cf1bc13c5cf8d2aa5f
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-11-19 12:16:54 +0200

    SERVICES-819: add new lines EOF

--- src/weekly-rewards-splitting/weekly-rewards-splitting.types.ts
+++ src/weekly-rewards-splitting/weekly-rewards-splitting.types.ts
@@ -4,11 +4,13 @@ export type UpdateUserEnergyEventType = {
     currentWeek: number;
     energy: EnergyType;
 };
+
 export type UpdateGlobalAmountsEventType = {
     currentWeek: number,
     totalLockedTokens: string,
     totalEnergy: string
 }
+
 export type ClaimMultiEventType = UpdateUserEnergyEventType & {
     allPayments: EsdtTokenPayment[]
 }
