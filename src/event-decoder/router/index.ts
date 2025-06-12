45af83e5cd8953333e732897a8b79ecae86f9172
hschiau <horia.schiau@multiversx.com>
2025-06-12 15:04:12 +0300

    MEX-819: add multi pair swap event for router

--- src/event-decoder/router/index.ts
+++ src/event-decoder/router/index.ts
@@ -2,3 +2,5 @@ export * from './createPair.event';
 export * from './pairSwapEnabled.event';
 export * from './createPair.topics';
 export * from './router.types';
+export * from './multiPairSwap.event';
+export * from './multiPairSwap.topics';
