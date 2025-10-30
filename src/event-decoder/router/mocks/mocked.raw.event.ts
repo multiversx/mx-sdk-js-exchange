d0c79ba3a18baf9c9f6836a444749b9a5d457010
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2025-10-30 17:44:52 +0200

    MEX-924: added unit test for create pair event decoder
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/event-decoder/router/mocks/mocked.raw.event.ts
+++ src/event-decoder/router/mocks/mocked.raw.event.ts
@@ -14,3 +14,16 @@ export const rawMultiPairSwapEvent: RawEventType = {
     ],
     data: 'AAAAAAAAAAAFAIEXZk8mIEPujRZ6vvH2E5U9A3aVVIMAAAAMV0VHTEQtYmQ0ZDc5AAAACAm25kqOxgAAAAAAC1VTREMtYzc2ZjFmAAAAA6E+jAAAAAEAAAALVVNEQy1jNzZmMWYAAAAAAAAAAAAAAAOhPowAAAAAAYY9DwAAAAAAAAbxAAAAAGhKc6w=',
 };
+
+export const rawCreatePairEvent: RawEventType = {
+    address: 'erd1qqqqqqqqqqqqqpgqa7hv0nahgsl8tz0psat46x0tchm0wuyc0n4s6q28ad',
+    identifier: 'createPair',
+    topics: [
+        'Y3JlYXRlX3BhaXI=',
+        'TklOSkFHTy1hYzdkNjY=',
+        'V0VHTEQtYTI4YzU5',
+        'yLDruv+uzy/JyNjcrIb7RYQI2OmLnNjq6VH78LcU5wQ=',
+        'EkE=',
+    ],
+    data: 'yLDruv+uzy/JyNjcrIb7RYQI2OmLnNjq6VH78LcU5wQAAAAOTklOSkFHTy1hYzdkNjYAAAAMV0VHTEQtYTI4YzU5AAAAAAAAASwAAAAAAAAAMgAAAAAAAAAABQC2lkBEc6f6v+pFplCk1zS6IVUPznzrAAAAAACrCLIAAAAAAAASQQAAAABpATQk',
+};
--- src/event-decoder/router/specs/router.event.spec.ts
+++ src/event-decoder/router/specs/router.event.spec.ts
@@ -1,4 +1,8 @@
-import { rawMultiPairSwapEvent } from '../mocks/mocked.raw.event';
+import { CreatePairEvent } from '../createPair.event';
+import {
+    rawCreatePairEvent,
+    rawMultiPairSwapEvent,
+} from '../mocks/mocked.raw.event';
 import { MultiPairSwapEvent } from '../multiPairSwap.event';
 
 describe('test router events decoder', () => {
@@ -25,6 +29,26 @@ describe('test router events decoder', () => {
                     amount: '10567308',
                 },
             ],
+        });
+    });
+
+    it('should decode create pair event', () => {
+        const createPairEvent = new CreatePairEvent(rawCreatePairEvent);
+        console.log(createPairEvent.toJSON());
+        expect(createPairEvent.toJSON()).toEqual({
+            address:
+                'erd1qqqqqqqqqqqqqpgqa7hv0nahgsl8tz0psat46x0tchm0wuyc0n4s6q28ad',
+            block: 11208882,
+            caller: 'erd1ezcwhwhl4m8jljwgmrw2ephmgkzq3k8f3wwd36hf28alpdc5uuzqunxua2',
+            epoch: 4673,
+            firstTokenID: 'NINJAGO-ac7d66',
+            identifier: 'createPair',
+            pairAddress:
+                'erd1qqqqqqqqqqqqqpgqk6tyq3rn5latl6j95eg2f4e5hgs42r7w0n4stt7kpw',
+            secondTokenID: 'WEGLD-a28c59',
+            specialFeePercent: 50,
+            timestamp: 1761686564,
+            totalFeePercent: 300,
         });
     });
 });
